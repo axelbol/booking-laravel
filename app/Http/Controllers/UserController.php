04082c25a45443d0e72a16d3f5a5f23db431914f
axelbol <axel_lp_alvarado@hotmail.com>
2021-01-12 11:35:56 -0400

    User validation when creating

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,6 +20,12 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|min:3|max:5',
+            'username' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
         $user = User::create($request->only('name', 'username', 'email')
             + [
                 'password' => bcrypt($request->input('password')),
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -12,28 +12,49 @@
               <p class="card-category">Ingresar datos</p>
             </div>
             <div class="card-body">
+              {{-- @if ($errors->any())
+                  <div class="alert alert-danger">
+                    <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                    </ul>
+                  </div>
+              @endif --}}
               <div class="row">
                 <label for="name" class="col-sm-2 col-form-label">Nombre</label>
                 <div class="col-sm-7">
-                  <input type="text" class="form-control" name="name" placeholder="Ingrese su nombre" autofocus>
+                  <input type="text" class="form-control" name="name" placeholder="Ingrese su nombre" value="{{ old('name') }}" autofocus>
+                  @if ($errors->has('name'))
+                    <span class="error text-danger" for="input-name">{{ $errors->first('name') }}</span>
+                  @endif
                 </div>
               </div>
               <div class="row">
                 <label for="username" class="col-sm-2 col-form-label">Nombre de usuario</label>
                 <div class="col-sm-7">
-                  <input type="text" class="form-control" name="username" placeholder="Ingrese su nombre de usuario">
+                  <input type="text" class="form-control" name="username" placeholder="Ingrese su nombre de usuario" value="{{ old('username') }}">
+                  @if ($errors->has('username'))
+                    <span class="error text-danger" for="input-username">{{ $errors->first('username') }}</span>
+                  @endif
                 </div>
               </div>
               <div class="row">
                 <label for="email" class="col-sm-2 col-form-label">Correo</label>
                 <div class="col-sm-7">
-                  <input type="email" class="form-control" name="email" placeholder="Ingrese su correo">
+                  <input type="email" class="form-control" name="email" placeholder="Ingrese su correo" value="{{ old('email') }}">
+                  @if ($errors->has('email'))
+                    <span class="error text-danger" for="input-email">{{ $errors->first('email') }}</span>
+                  @endif
                 </div>
               </div>
               <div class="row">
                 <label for="password" class="col-sm-2 col-form-label">Contraseña</label>
                 <div class="col-sm-7">
                   <input type="password" class="form-control" name="password" placeholder="Contraseña">
+                  @if ($errors->has('password'))
+                    <span class="error text-danger" for="input-password">{{ $errors->first('password') }}</span>
+                  @endif
                 </div>
               </div>
             </div>
