9534fd40214fb651cb2580796a97f12930fcc103
axelbol <axel_lp_alvarado@hotmail.com>
2021-05-17 21:06:02 -0400

    show & delete roles w/ permissions

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -36,7 +36,11 @@
                       </div>
                     </p>
                     <div class="card-description">
-                      {{ _('Do not be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...') }}
+                      @forelse ($role->permissions as $permission)
+                          <span class="badge rounded-pill bg-dark text-white">{{ $permission->name }}</span>
+                      @empty
+                          <span class="badge badge-danger bg-danger">No permissions</span>
+                      @endforelse
                     </div>
                   </div>
                   <div class="card-footer">
